db17e003b8ffe5eb132c3233c2fcc69e5face1ed
Mike H <76085826+mhulse11@users.noreply.github.com>
2021-02-19 11:01:44 -0700

    changes from 19-17

--- html/js/index.js
+++ html/js/index.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
     }, function(){
     $(this).css("background-color", "#333333");
   });
+  $(".navitem").hover(function(){
+    $(this).css("background-color", "#FF5F5F");
+    }, function(){
+    $(this).css("background-color", "#333333");
+  });
 });
+
+ 
